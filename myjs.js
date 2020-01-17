0af005ded0700d47eba33233e9261a51c5b1e53b
ajbot2019 <ajitantny@gmail.com>
2020-01-17 23:00:17 +0530

    Add all

---
+++ myjs.js
@@ -0,0 +1,21 @@
+let name = prompt('what your name buddy?');
+document.getElementById('welcome').innerHTML = 'good morning  ' + name;
+
+
+console.log('hello')
+console.log(444)
+
+let learning ='im learning JS'
+console.log(learning)
+
+console.log([1,2,3])
+
+let myinfo={name:"jsajith",age:"15"}
+console.log(myinfo)
+
+//console.timeStart('start time')
+console.table(['a','b','c','d'])
+console.error('error is in red')
+console.warn('warnin9g is in yello')
+console.timeEnd('end time')
+console.clear();
